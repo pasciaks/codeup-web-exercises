f048b8a9a40320dbdee30f649265f76332525a74
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-12 10:38:22 -0600

    Saved JS

--- js/external.js
+++ js/external.js
@@ -1,21 +1,2 @@
 console.log('Started...');
 
-console.log(`
-console.log(13 % 5);
-console.log(1 % -2);
-console.log(1 % 2);
-console.log(2 % 3);
-console.log(5.5 % 2);
-console.log(3n % 2n);
-console.log(3n / 2n);
-`);
-
-console.log(13 % 5);
-console.log(13 % 5);
-console.log(1 % -2);
-console.log(1 % 2);
-console.log(2 % 3);
-console.log(5.5 % 2);
-console.log(3n % 2n);
-console.log(3n / 2n);
-
