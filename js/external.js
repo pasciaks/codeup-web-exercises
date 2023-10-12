95fd28380c0cee7f3e4eef83c1b7c0d9b63328c3
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-12 13:05:21 -0600

    Number and other working with types

--- js/external.js
+++ js/external.js
@@ -7,3 +7,26 @@ let b = ++a;
 let c = ++a;
 
 console.log({a, b, c});
+
+let x = 1;
+++x; // pre-increment, x is incremented, and the console prints out 2
+console.log(x); // 2
+
+x = 1; // put x back to where we started
+x++; // post-increment, console prints out 1, and the value in x is incremented
+console.log(x); // 2
+
+console.log(parseInt('123abc'));// 123)
+
+let zz = 'sheldon';
+
+zz -= "s";
+
+console.log(zz); // NaN
+
+let aNumber = 5;
+let aString = "" + aNumber;
+
+console.log(Number('5'));
+
+console.log(typeof NaN);
