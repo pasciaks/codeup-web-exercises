23bf2770df74b3e6c36b586e2379020c86a54703
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-16 08:43:01 -0600

    Update example.

--- js/functions_practice.js
+++ js/functions_practice.js
@@ -10,6 +10,11 @@ function helloSomeone(name) {
     return `Hello, ${name}!`;
 }
 
+/**
+ * helloAnyone - returns a greeting string given a passed in name.
+ * @param name
+ * @returns {string}
+ */
 function helloAnyone(name = "Unknown") {
     return `Hello, ${name}!`;
 }
@@ -23,3 +28,11 @@ console.log(helloSomeoneResponse);
 let helloAnyoneResponse = helloAnyone();
 console.log(helloAnyoneResponse);
 
+function addNumbers(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+let a = 1;
+let b = 2;
+let c = addNumbers(a, b);
+console.log(`${a} + ${b} = ${c}`);
