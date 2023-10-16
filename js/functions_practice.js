846ef6d4df94269fc2e016e4a371ce73f4df5958
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-16 09:02:43 -0600

    Functions notes and comments.

--- js/functions_practice.js
+++ js/functions_practice.js
@@ -2,17 +2,26 @@
 
 console.log("Hello from functions_practice.js JavaScript!");
 
+/**
+ * helloWorld - Returns the Hello World! greeting string
+ * @returns {string}
+ */
 function helloWorld() {
     return 'Hello World!';
 }
 
+/**
+ * helloSomeone - Returns a greeting string given a passed in name.
+ * @param name
+ * @returns {string}
+ */
 function helloSomeone(name) {
     return `Hello, ${name}!`;
 }
 
 /**
- * helloAnyone - returns a greeting string given a passed in name.
- * @param name
+ * helloAnyone - Returns a greeting string given a passed in name.
+ * @param name - Defaults to 'Unknown' if no value is passed in.
  * @returns {string}
  */
 function helloAnyone(name = "Unknown") {
@@ -36,3 +45,17 @@ let a = 1;
 let b = 2;
 let c = addNumbers(a, b);
 console.log(`${a} + ${b} = ${c}`);
+
+/**
+ * doSomething - Stores anonymous function for use later, Displays console message Done!
+ * @returns {null}
+ */
+let doSomething = function () {
+    console.log("Done!");
+    return null;
+}
+
+doSomething();
+console.log(doSomething);
+console.log(doSomething());
+
