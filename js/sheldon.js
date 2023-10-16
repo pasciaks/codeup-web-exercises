3f4fc537ce5a47ae6f266999f7cc56ed15af62e1
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-16 14:50:47 -0600

    Misc comments.

--- js/sheldon.js
+++ js/sheldon.js
@@ -1,1 +1,24 @@
 "use strict";
+
+isTwo(2);
+
+let myName = 'Sheldon';
+
+console.log(myName);
+
+function isTwo(num) { // Note: This function will be hoisted!
+    return num === 2;
+}
+
+const myFunction = function () {
+    return '';
+}
+
+const fatArrow = () => {
+    return '';
+}
+
+myFunction()
+
+
+
