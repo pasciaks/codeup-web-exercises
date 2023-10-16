59b7b7a91a62411b69ea00073608af927bdf505b
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-16 09:08:01 -0600

    Additional comments.

--- js/functions_practice.js
+++ js/functions_practice.js
@@ -37,6 +37,12 @@ console.log(helloSomeoneResponse);
 let helloAnyoneResponse = helloAnyone();
 console.log(helloAnyoneResponse);
 
+/**
+ * addNumbers - Returns the Number sum of two arguments passed in
+ * @param num1
+ * @param num2
+ * @returns {number}
+ */
 function addNumbers(num1, num2) {
     return Number(num1) + Number(num2);
 }
