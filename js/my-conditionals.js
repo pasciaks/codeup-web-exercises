0cefec285d7affaf4dfa9191cce9315677b06807
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-17 09:37:35 -0600

    Misc.

--- js/my-conditionals.js
+++ js/my-conditionals.js
@@ -5,20 +5,23 @@ console.log("My Conditionals Started.");
 let milk = 2000;
 let durationMilliseconds = 3000;
 
-const drinkMilk = (amount) => {
+const drinkMilk = (amount = 0) => {
     milk -= amount;
 }
 
-const getMilk = (amount) => {
+const getMilk = (amount = 0) => {
     milk += amount;
 }
 
-const startedAt = Date.now();
 let currentTime = Date.now();
+
+const startedAt = Date.now();
 
 do {
 
     let random = Math.floor(Math.random() * (3) + 1);
+
+    console.log(`Random number: [${random}]`);
 
     if (random === 1) {
         drinkMilk(1 + Math.random() * 5);
