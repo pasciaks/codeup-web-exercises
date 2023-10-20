ff3412a2dbd82884f16fdcd36ecc9f33603114bf
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-20 13:05:57 -0600

    Destructure array into function args

--- js/my-arrays.js
+++ js/my-arrays.js
@@ -1,6 +1,6 @@
 let emptyArr = [];
 
-// console.log('isArray ? ' + Array.isArray(emptyArr));
+console.log('isArray ? ' + Array.isArray(emptyArr));
 
 let falsy = [0, "", false, undefined, null];
 
@@ -35,12 +35,59 @@ for (let i = 0; i < cheeses.length; i++) {
     console.log(cheeses[i]);
 }
 
+console.log('forEach(callBack)');
+cheeses.forEach(function (cheese) {
+    console.log(cheese);
+})
+
+console.log('forEach((item)={}))');
 cheeses.forEach((cheese) => {
     console.log(cheese);
 })
+
+console.log('forEach((item,index)={}))');
+
 cheeses.forEach((cheese, index) => {
     console.log(cheese, index);
+    console.log(`${index}: ${cheese}`);
 })
+
+console.log('forEach((item,index,array)={}))');
+
 cheeses.forEach((cheese, index, array) => {
     console.log(cheese, index, array);
 })
+
+console.log(cheeses);
+cheeses[1] = 'String (string-cheese)';
+console.log(cheeses);
+
+console.log("let cheese of cheeses");
+for (let cheese of cheeses) {
+    console.log(cheese);
+}
+
+const array = [15, 16, 17, 18, 19];
+
+console.log("Reduce function...");
+
+let s = 15 + 16 + 17 + 18 + 19;
+console.log(s);
+
+let sum = array.reduce(function (acc, value, index, arr) {
+    console.log(acc);
+    console.log(value);
+    console.log(index);
+    console.log(arr);
+    return acc + value;
+}, 0);
+
+console.log(sum);
+
+console.log('---- spread operator ----');
+console.log(...cheeses);
+console.log(JSON.stringify([...cheeses], 0, 2));
+
+const exampleVals = [5, 8];
+const add = (x, y) => x + y;
+console.log(exampleVals, add(...exampleVals));
