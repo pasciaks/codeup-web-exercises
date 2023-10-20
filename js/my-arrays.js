7c39694b659b9ac0691b3a4f6a61902fa090c7ad
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-20 13:27:37 -0600

    Finished arrays iterating.

--- js/my-arrays.js
+++ js/my-arrays.js
@@ -93,3 +93,69 @@ console.log(JSON.stringify([...cheeses], 0, 2));
 const exampleVals = [5, 8];
 const add = (x, y) => x + y;
 console.log(exampleVals, add(...exampleVals));
+
+const daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
+
+console.log(daysOfTheWeek);
+// ['Monday', 'Tuesday', 'Wednesday', 'Thursday']
+
+// let's add 'Sunday' to the beginning of the week
+daysOfTheWeek.unshift('Sunday');
+
+console.log(daysOfTheWeek);
+// ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
+
+// let's add 'Friday' and 'Saturday' to the end of the week
+daysOfTheWeek.push('Friday', 'Saturday');
+
+console.log(daysOfTheWeek);
+// ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+/**
+ * Array Mutation
+ * Both push and unshift will modify the original array,
+ * and return a number, the new length of the array,
+ * not the new array.
+ */
+
+const todoList = ['Take out the trash', 'Clean the car', 'Pay the bills'];
+
+console.log('My todo list:');
+console.log(todoList);
+// ['Take out the trash', 'Clean the car', 'Pay the bills']
+
+console.log('Completing the last item: ' + todoList[todoList.length - 1]);
+
+// let's remove the last item
+const removedItem = todoList.pop();
+
+// log what we did
+console.log('Task complete: ' + removedItem);
+
+console.log(todoList);
+// ['Take out the trash', 'Clean the car']
+
+console.log('Completing the first item: ' + todoList[0]);
+
+// let's remove the first item
+const doneItem = todoList.shift();
+
+// log what we did
+console.log('Task complete: ' + doneItem);
+
+console.log(todoList);
+// ['Clean the car']
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+console.log(colors); // colors is unchanged
+
+let slice = colors.slice(2, 4);
+console.log(slice); // ['yellow', 'green']
+
+console.log(colors); // colors is unchanged
+// ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+
+slice = colors.slice(3);
+console.log(slice); // ['green', 'blue', 'indigo', 'violet']
