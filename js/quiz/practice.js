b5ed3f9d3991a3bfa95af651788abb1749e9f9f9
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-23 09:16:00 -0600

    Git exercises. array lessons.

---
+++ js/quiz/practice.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("Started...");
+
+// var i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+// console.log(10 % 3);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
