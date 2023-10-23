c29356a8708f8cae94367e50fe205a71f58f7081
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-23 15:45:20 -0600

    Calc age function on object.

--- js/quiz/practice.js
+++ js/quiz/practice.js
@@ -11,3 +11,27 @@ while (i <= 10) {
 for (let i = 0; i <= 10; i++) {
     console.log(i);
 }
+
+const user = {
+    firstName: 'Sheldon',
+    d_o_b: new Date(1970, 1, 15),
+    age: null
+}
+
+user.calcAge = function () {
+    const today = new Date();
+    if (this.d_o_b.getMonth() < today.getMonth()) {
+        if (this.d_o_b.getDate() < today.getDate()) {
+            this.age = today.getFullYear() - this.d_o_b.getFullYear();
+        } else {
+            this.age = today.getMonth() - this.d_o_b.getFullYear() - 1;
+        }
+    } else {
+        this.age = today.getFullYear() - this.d_o_b.getFullYear() - 1;
+    }
+    return this.age;
+}
+
+console.log(user);
+user.calcAge();
+console.log(user);
