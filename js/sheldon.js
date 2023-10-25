45eaa265e49d61cd985ac1d523e2ccfc0604fba7
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-25 10:27:53 -0600

    Misc github script

--- js/sheldon.js
+++ js/sheldon.js
@@ -28,6 +28,47 @@ const fatArrow = () => {
     return {'isObject': true};
 }
 
+/**
+ * # while read in; do ./script.sh "$in" "break_and_continue.js" ; done < list.txt
+ * # while read in; do ./script.sh "$in" "circle.js" ; done < list.txt
+ * # while read in; do ./script.sh "$in" "for_loops.js" ; done < list.txt
+ */
+
+/**
+ * radiancelux
+ * pasciaks
+ * davidandstephens
+ * derykdamon
+ * eltownes
+ * GBowman1
+ * psykter
+ * mventura14
+ * BryonLuke
+ * mbcodepractice
+ * RicardoJaimesDez
+ * cteriipaia
+ * Santoszls3
+ * AngelAdell
+ * IraHuff
+ * AmritLee
+ * TheCosmicFish
+ * MichaelaArteberry
+ * Cmgrowald
+ * Yadriel-Ruiz
+ * easi-d-hatchett
+ * sethcrist
+ * Geoffrey-m-rendon
+ * ben-w-martin
+ */
+
+/**
+ * curl -o $1-$2 -LO https://raw.githubusercontent.com/$1/codeup-web-exercises/main/js/$2
+ * # while read in; do ./script.sh "$in" "break_and_continue.js" ; done < list.txt
+ * # while read in; do ./script.sh "$in" "circle.js" ; done < list.txt
+ * # while read in; do ./script.sh "$in" "for_loops.js" ; done < list.txt
+ * # while read in; do ./script.sh "$in" "external.js" ; done < list.txt
+ * # while read in; do ./script.sh "$in" "conditionals.js" ; done < list.txt
+ */
 console.log(typeof isTwo);
 console.log(typeof myFunction)
 console.log(typeof fatArrow)
