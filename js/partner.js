06a6615c47e696bca515d154249c73bf9c68d119
Sheldon Pasciak <sheldon@pasciak.com>
2023-11-07 14:37:56 -0700

    Misc

--- js/partner.js
+++ js/partner.js
@@ -25,8 +25,6 @@ function parterMaker(ts = 2) {
         "ben-w-martin"
     ]
 
-    console.log('\n\n\n\n\n\n\n\n\n\n');
-
     let teams = {};
     let originalLength = gitProfiles.length;
     let count = 0;
@@ -37,7 +35,6 @@ function parterMaker(ts = 2) {
     do {
         let choice = Math.floor(Math.random() * (gitProfiles.length))
         console.log(gitProfiles[choice]);
-
         if (!teams[`${teamIndex}`]) {
             teams[`${teamIndex}`] = [];
         }
@@ -52,8 +49,6 @@ function parterMaker(ts = 2) {
         }
     } while (gitProfiles.length > 0)
 
-    console.log('\n\n\n\n\n\n\n\n\n\n');
-
     console.log("Team Size");
     console.log(teamSize);
 
@@ -63,12 +58,10 @@ function parterMaker(ts = 2) {
     console.log(`Whole teams of ${teamSize} members`);
     console.log(Math.floor((originalLength / teamSize)));
 
-
     console.log("Partial Team");
     console.log(((originalLength / teamSize) - Math.floor((originalLength / teamSize))) * teamSize);
 
     console.log(teams);
-
-
+    
     return teams;
 }
