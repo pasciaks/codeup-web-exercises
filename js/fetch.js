900cf898059fe4e058d283ab53d45eae64ac39d0
Sheldon Pasciak <sheldon@pasciak.com>
2023-11-28 09:34:48 -0700

    Github latest commit testing

--- js/fetch.js
+++ js/fetch.js
@@ -1,51 +1,98 @@
-// The GitHub token is stored local in my computer notes
+"use strict";
 
-function readGithubUserData(githubUsername, personalAccessToken) {
+(() => {
 
-    // https://api.github.com/users/pasciaks/events
-    // https://api.github.com/users/pasciaks/repos
-    // https://api.github.com/users/pasciaks/orgs
-    // https://api.github.com/users/pasciaks/starred
-    // https://api.github.com/users/pasciaks/subscriptions
-    // https://api.github.com/users/pasciaks/followers
-    // https://api.github.com/users/pasciaks/commits
+    // The below code is kept here for important reference during this lesson.
 
-    return fetch(`https://api.github.com/users/${githubUsername}/commits`, {
-        headers: {
-            'Authorization': personalAccessToken
-        }
-    })
+    // fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+    //     .then(response => {
+    //         // can't use the response's data yet. have to json() it
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         // at this point, we can use the response's data
+    //         console.log(data);
+    //     });
 
-}
 
-let githubUsername = prompt("Enter your GitHub username");
+    function readGithubUserData(githubUsername, personalAccessToken) {
 
-let githubPersonalAccessToken = prompt("Enter your GitHub token");
+        // https://api.github.com/users/pasciaks/events
+        // https://api.github.com/users/pasciaks/repos
+        // https://api.github.com/users/pasciaks/orgs
+        // https://api.github.com/users/pasciaks/starred
+        // https://api.github.com/users/pasciaks/subscriptions
+        // https://api.github.com/users/pasciaks/followers
+        // https://api.github.com/users/pasciaks/commits
 
-let getData = readGithubUserData(githubUsername, githubPersonalAccessToken);
-getData.then((data) => {
-    console.log(data)
-}).catch((error) => {
-    console.log(error)
-});
+        let url;
 
-// MISC examples
+        url = `https://api.github.com/users/${githubUsername}/events`;
 
-// const githubPromise = fetch('https://api.github.com/repositories');
-// const bitbucketPromise = fetch('https://api.bitbucket.org/2.0/repositories');
-//
-// Promise.all([githubPromise, bitbucketPromise])
-//     .then(data => {
-//         // here data is an array of the resolved values from each promise
-//         // we can now do something with both pieces of data
-//         return data;
-//     })
-//     .then((data) => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         // handle errors
-//         console.log(error);
-//     });
+        return fetch(url, {
+            headers: {
+                'Authorization': personalAccessToken
+            }
+        });
+
+
+    }
+
+    document.getElementById('fetch-button').addEventListener('click', () => {
+        let githubUsername = prompt("Enter your GitHub username", "pasciaks");
+        let githubPersonalAccessToken = prompt("Enter your GitHub token, Do not store this in your code!", "");
+        let getData = readGithubUserData(githubUsername, githubPersonalAccessToken);
+
+        let events = {};
+
+        getData
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                let arrayOfEvents = Object.keys(data);
+                arrayOfEvents.forEach((event) => {
+                    let currentEvent = data[event];
+                    if (!events[currentEvent.type]) {
+                        events[currentEvent.type] = [];
+                        events[currentEvent.type].push(currentEvent);
+                    } else {
+                        events[currentEvent.type].push(currentEvent);
+                    }
+                });
+                return events;
+            })
+            .then((events) => {
+                return events['PushEvent'];
+            })
+            .then((pushEvents) => {
+                pushEvents.forEach((pushEvent, index) => {
+                    if (index === 0) {
+                        console.log("Most recent push event:");
+                        console.log(pushEvent);
+                        console.log(pushEvent.created_at);
+
+                        document.getElementById('fetch-output').innerHTML = `
+                        <h2>Most recent push event:</h2>
+                        <p>Created at: ${pushEvent.created_at}</p>
+                        <p>Repo name: ${pushEvent.repo.name}</p>
+                        <p>Commit message: ${pushEvent.payload.commits[0].message}</p>
+                        <p>Event: ${JSON.stringify(pushEvent)}</p>
+                        `;
+                    }
+                });
+            })
+            .catch((error) => {
+                return error;
+            });
+    });
+
+
+})();
+
+
+
+
+
 
 
