e39ced5dcbafba92d1096ecbe21c1219f83141e9
Sheldon Pasciak <sheldon@pasciak.com>
2023-11-30 14:49:36 -0700

    Good starter mapbox library

---
+++ js/mapbox-geocoder-utils.js
@@ -0,0 +1,252 @@
+"use strict";
+
+// https://codeup-curriculum.web.app/curriculum/java/3.0.1/javascript-3/mapbox-api
+// https://docs.mapbox.com/mapbox-gl-js/api/
+// https://docs.mapbox.com/mapbox-gl-js/example/scroll-fly-to/
+// https://docs.mapbox.com/mapbox-gl-js/example/
+
+// references: https://jsfiddle.net/jz08vuo4/
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-111.9462511, 40.6466734], // starting position [lng, lat]
+    zoom: 14, // starting zoom
+});
+
+let dynamicallyAddedMapObjectsArray = [];
+
+/**
+ * setCurrentPosition is a method to set the current position of the map to the user's current location
+ *
+ * @returns {Promise<void>}
+ */
+const setCurrentPosition = /* async */ () => {
+    /* await */
+    navigator.geolocation.getCurrentPosition(function (navPosObj) {
+        try {
+            localStorage.setItem('slp_nav_lat', navPosObj.coords.latitude);
+            localStorage.setItem('slp_nav_lng', navPosObj.coords.longitude);
+            sessionStorage.setItem('slp_nav_lat', navPosObj.coords.latitude);
+            sessionStorage.setItem('slp_nav_lng', navPosObj.coords.longitude);
+            console.log("Latitude is :", navPosObj.coords.latitude);
+            console.log("Longitude is :", navPosObj.coords.longitude);
+            map.flyTo({center: [navPosObj.coords.longitude, navPosObj.coords.latitude], zoom: 18});
+        } catch {
+            console.log("Not able to obtain geo coords.");
+        }
+    });
+}
+
+/***
+ * geocode is a method to search for coordinates based on a physical address and return
+ *
+ * @param {string} search is the address to search for the geocoded coordinates
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the latitude and longitude as a two element array
+ *
+ * EXAMPLE:
+ *
+ *  geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+function geocode(search, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+
+    let uriEncodedSearch = encodeURIComponent(search);
+    let uriDecodedSearch = decodeURIComponent(uriEncodedSearch);
+
+    console.log("uriEncodedSearch: " + uriEncodedSearch);
+    console.log("uriDecodedSearch: " + uriDecodedSearch);
+
+    return fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
+        .then(res => res.json())
+        // to get all the data from the request, comment out the following three lines...
+        .then(data => data.features[0].center);
+}
+
+/***
+ * reverseGeocode is a method to search for a physical address based on inputted coordinates
+ *
+ * @param {object} coordinates is an object with properties "lat" and "lng" for latitude and longitude
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the string of the closest matching location to the coordinates provided
+ *
+ * EXAMPLE:
+ *
+ *  reverseGeocode({lat: 32.77, lng: -96.79}, API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+function reverseGeocode(coordinates, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
+        .then(res => res.json())
+        // to get all the data from the request, comment out the following three lines...
+        .then(data => data.features[0].place_name);
+}
+
+
+// the geocode method from mapbox-geocoder-utils.js
+// geocode("600 Navarro St #350, San Antonio, TX 78205", MAPBOX_TOKEN).then(result => {
+//     console.log(result);
+//     map.setCenter(result);
+//     map.setZoom(15);
+// });
+
+// the reverse geocode method from mapbox-geocoder-utils.js
+// reverseGeocode({lng: -98.4861, lat: 29.4260}, MAPBOX_TOKEN).then(results => {
+//         console.log(results);
+//         alert(JSON.stringify(results));
+//     }
+// );
+
+function placeMarkerAndPopupUsingAddress(address, popupHTML, token, map, draggable = false) {
+    let id = Date.now() + Math.floor(Math.random() * 99999);
+    geocode(address, token)
+        .then(coords => {
+            console.log(coords);
+            let popup = new mapboxgl.Popup()
+                .setHTML(popupHTML);
+            let marker = new mapboxgl.Marker({
+                draggable
+            })
+                .setLngLat(coords)
+                .addTo(map)
+                .setPopup(popup);
+            if (draggable) {
+                function onDragEnd(e) {
+                    const lngLat = e.target.getLngLat();
+                    console.log(e.target);
+                    console.log(lngLat);
+                }
+
+                marker.on('dragend', onDragEnd);
+            }
+            popup.addTo(map);
+            dynamicallyAddedMapObjectsArray.push({id, popup, marker});
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    return id;
+}
+
+function placeMarkerAndPopupUsingCoords(coords, popupHTML, token, map, draggable = false) {
+    let id = Date.now() + Math.floor(Math.random() * 99999);
+    let popup = new mapboxgl.Popup()
+        .setHTML(popupHTML);
+    let marker = new mapboxgl.Marker({
+        draggable
+    })
+        .setLngLat(coords)
+        .addTo(map)
+        .setPopup(popup);
+    if (draggable) {
+        function onDragEnd(e) {
+            const lngLat = e.target.getLngLat();
+            console.log(e.target);
+            console.log(lngLat);
+        }
+
+        marker.on('dragend', onDragEnd);
+    }
+    dynamicallyAddedMapObjectsArray.push({id, popup, marker});
+    popup.addTo(map);
+    return id;
+}
+
+function executeTests(id = 0) {
+    switch (id) {
+        case 1:
+            setTimeout(function () {
+                map.setZoom(14.34); //set map zoom level for desktop size
+            }, 3000);
+            break;
+
+        case 2:
+            setTimeout(function () {
+                map.setZoom(11); //set map zoom level for mobile size
+            }, 3000);
+            break;
+
+        case 3:
+            setTimeout(function () {
+                setCurrentPosition();
+            }, 3000);
+            break;
+
+        case 4:
+            let address = "2174 W Fairwood Dr, Taylorsville, UT 84129";
+            let popupHTML = "<p>Welcome to the 2174 W Fairwood Dr, Taylorsville, UT 84129!</p>";
+            placeMarkerAndPopupUsingAddress(address, popupHTML, MAPBOX_TOKEN, map, false);
+            break;
+
+        case 5:
+            let popupLocationCoordsArray = [-111.9462511, 40.6466734];
+            let popupCoordsHTML = `<div>${JSON.stringify(popupLocationCoordsArray, null, 2)}</div>\n`;
+            console.log(popupLocationCoordsArray, popupCoordsHTML);
+            placeMarkerAndPopupUsingCoords(popupLocationCoordsArray, popupCoordsHTML, MAPBOX_TOKEN, map, false);
+            break;
+
+        case 6:
+            geocode("548 W 4500 S, Murray, UT 84123", MAPBOX_TOKEN)
+                .then(result => {
+                    console.log(result, "548 W 4500 S, Murray, UT 84123");
+                    map.setCenter(result);
+                    map.setZoom(18);
+                });
+            break;
+
+        case 7:
+            reverseGeocode({lng: -98.4861, lat: 29.4260}, MAPBOX_TOKEN)
+                .then(results => {
+                    console.log(results);
+                    map.setCenter([-98.4861, 29.4260]);
+                    map.setZoom(15);
+                });
+            break;
+
+        default:
+            break;
+    }
+}
+
+let testId = 0;
+
+executeTests(testId);
+
+setTimeout(function () {
+    dynamicallyAddedMapObjectsArray.forEach((mapObject) => {
+        console.log(mapObject);
+        let clickableItem = mapObject.marker.getElement();
+        console.log(clickableItem);
+        clickableItem.addEventListener('click', function (e) {
+            console.log("marker clicked:", mapObject);
+        });
+    });
+}, 2000);
+
+setTimeout(function () {
+    dynamicallyAddedMapObjectsArray.forEach((mapObject) => {
+        console.log(mapObject);
+        mapObject.marker.togglePopup();
+    });
+}, 3000);
+
+setTimeout(function () {
+    dynamicallyAddedMapObjectsArray.forEach((mapObject) => {
+        console.log(mapObject);
+        mapObject.marker.togglePopup();
+    });
+}, 4000);
+
+
+
--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -23,29 +23,29 @@ Replace the generic marker icon with an image that is more appropriate for each 
 
  */
 
-console.log("mapbox-test.js is connected...");
+// console.log("mapbox-test.js is connected...");
 
-mapboxgl.accessToken = MAPBOX_TOKEN;
+// mapboxgl.accessToken = MAPBOX_TOKEN;
 
-const setCurrentPosition = async () => {
-    await navigator.geolocation.getCurrentPosition(function (navPosObj) {
-        console.log("Latitude is :", navPosObj.coords.latitude);
-        console.log("Longitude is :", navPosObj.coords.longitude);
-        console.log("Latitude is :" + navPosObj.coords.latitude + " Longitude is :" + navPosObj.coords.longitude);
-
-        try {
-            localStorage.setItem('slp_nav_lat', navPosObj.coords.latitude);
-            localStorage.setItem('slp_nav_lng', navPosObj.coords.longitude);
-            sessionStorage.setItem('slp_nav_lat', navPosObj.coords.latitude);
-            sessionStorage.setItem('slp_nav_lng', navPosObj.coords.longitude);
-
-            map.flyTo({center: [navPosObj.coords.longitude, navPosObj.coords.latitude]});
-
-        } catch {
-            console.log("Not able to obtain geo coords.");
-        }
-    });
-}
+// const setCurrentPosition = async () => {
+//     await navigator.geolocation.getCurrentPosition(function (navPosObj) {
+//         console.log("Latitude is :", navPosObj.coords.latitude);
+//         console.log("Longitude is :", navPosObj.coords.longitude);
+//         console.log("Latitude is :" + navPosObj.coords.latitude + " Longitude is :" + navPosObj.coords.longitude);
+//
+//         try {
+//             localStorage.setItem('slp_nav_lat', navPosObj.coords.latitude);
+//             localStorage.setItem('slp_nav_lng', navPosObj.coords.longitude);
+//             sessionStorage.setItem('slp_nav_lat', navPosObj.coords.latitude);
+//             sessionStorage.setItem('slp_nav_lng', navPosObj.coords.longitude);
+//
+//             map.flyTo({center: [navPosObj.coords.longitude, navPosObj.coords.latitude]});
+//
+//         } catch {
+//             console.log("Not able to obtain geo coords.");
+//         }
+//     });
+// }
 
 /***
  * geocode is a method to search for coordinates based on a physical address and return
@@ -60,15 +60,21 @@ const setCurrentPosition = async () => {
  *  })
  *
  */
-function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
-        .then(res => res.json())
-        // to get all the data from the request, comment out the following three lines...
-        .then(data => data.features[0].center);
-}
-
+// function geocode(search, token) {
+//     let baseUrl = 'https://api.mapbox.com';
+//     let endPoint = '/geocoding/v5/mapbox.places/';
+//
+//     let uriEncodedSearch = encodeURIComponent(search);
+//     let uriDecodedSearch = decodeURIComponent(uriEncodedSearch);
+//
+//     console.log("uriEncodedSearch: " + uriEncodedSearch);
+//     console.log("uriDecodedSearch: " + uriDecodedSearch);
+//
+//     return fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
+//         .then(res => res.json())
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(data => data.features[0].center);
+// }
 
 /***
  * reverseGeocode is a method to search for a physical address based on inputted coordinates
@@ -83,21 +89,21 @@ function geocode(search, token) {
  *  })
  *
  */
-function reverseGeocode(coordinates, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
-        .then(res => res.json())
-        // to get all the data from the request, comment out the following three lines...
-        .then(data => data.features[0].place_name);
-}
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
+//         .then(res => res.json())
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(data => data.features[0].place_name);
+// }
 
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-});
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: [-74.5, 40], // starting position [lng, lat]
+//     zoom: 9, // starting zoom
+// });
 
 // setCurrentPosition();
 
@@ -115,30 +121,30 @@ const map = new mapboxgl.Map({
 //     }
 // );
 
-function placeMarkerAndPopup(info, token, map) {
-    geocode(info.address, token).then(coords => {
-        let popup = new mapboxgl.Popup()
-            .setHTML(info.popupHTML);
-        let marker = new mapboxgl.Marker({
-            draggable: true
-        })
-            .setLngLat(coords)
-            .addTo(map)
-            .setPopup(popup);
-
-        function onDragEnd(e) {
-
-            const lngLat = e.target.getLngLat();
-            console.log(e.target);
-            console.log(lngLat);
-        }
-
-        marker.on('dragend', onDragEnd);
-
-        popup.addTo(map);
-
-    });
-}
+// function placeMarkerAndPopup(info, token, map) {
+//     geocode(info.address, token).then(coords => {
+//         let popup = new mapboxgl.Popup()
+//             .setHTML(info.popupHTML);
+//         let marker = new mapboxgl.Marker({
+//             draggable: true
+//         })
+//             .setLngLat(coords)
+//             .addTo(map)
+//             .setPopup(popup);
+//
+//         function onDragEnd(e) {
+//
+//             const lngLat = e.target.getLngLat();
+//             console.log(e.target);
+//             console.log(lngLat);
+//         }
+//
+//         marker.on('dragend', onDragEnd);
+//
+//         popup.addTo(map);
+//
+//     });
+// }
 
 // const alamoInfo = {
 //     address: "The Alamo, San Antonio",
@@ -147,6 +153,16 @@ function placeMarkerAndPopup(info, token, map) {
 //
 // placeMarkerAndPopup(alamoInfo, MAPBOX_TOKEN, map);
 
+// setTimeout(function () {
+//     map.setZoom(14.34); //set map zoom level for desktop size
+// }, 15000);
+
+// setTimeout(function () {
+//     map.setZoom(11); //set map zoom level for mobile size
+// }, 5000);
 
 
+///----
+// https://jsfiddle.net/jz08vuo4/
+///----
 
