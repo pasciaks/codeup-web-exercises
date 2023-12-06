e609a1d2656aa16ae67453b0c099b14274915d9f
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-06 09:10:36 -0700

    JSON server start

---
+++ js/mock-api-test.mjs
@@ -0,0 +1,144 @@
+// // index.js
+// fetch('http://localhost:3000/books')
+//     .then(response => response.json())
+//     .then(books => console.log(books))
+//     .catch(error => console.error(error));
+//
+// // index.js
+// fetch('http://localhost:3000/books/1')
+//     .then(response => response.json())
+//     .then(book => {
+//         // do something with the `book` data
+//         console.log(book);
+//     })
+//     .catch(error => console.error(error));
+//
+// // index.js
+// fetch('http://localhost:3000/books/1')
+//     .then(response => response.json())
+//     .then(book => {
+//         // A second fetch request to get the author data
+//         fetch(`http://localhost:3000/authors/${book.authorId}`)
+//             .then(response => response.json())
+//             .then(author => {
+//                 // Log the book and author data to the console
+//                 console.log(book);
+//                 console.log(author);
+//                 // OR we could simply add the author into the book object
+//                 book.author = author;
+//                 console.log(book);
+//             })
+//             .catch(error => console.error(error));
+//     })
+//     .catch(error => console.error(error));
+//
+//
+// // index.js
+// const getBookAndAuthor = async (id) => {
+//     try {
+//         // Get the book data
+//         const bookUrl = `http://localhost:3000/books/${id}`;
+//         const bookResponse = await fetch(bookUrl);
+//         const book = await bookResponse.json();
+//         // Get the author data
+//         const authorUrl = `http://localhost:3000/authors/${book.authorId}`;
+//         const authorResponse = await fetch(authorUrl);
+//         const author = await authorResponse.json();
+//         // Add the author to the book object
+//         book.author = author;
+//         // Return the book object
+//         return book;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+//
+//
+//
+//
+//
+//
+
+// index.js
+// const getBookAndAuthor = async (id) => {
+//     try {
+//         // Get the book data
+//         const bookUrl = `http://localhost:3000/books/${id}`;
+//         const bookResponse = await fetch(bookUrl);
+//         const book = await bookResponse.json();
+//         // Get the author data
+//         const authorUrl = `http://localhost:3000/authors/${book.authorId}`;
+//         const authorResponse = await fetch(authorUrl);
+//         const author = await authorResponse.json();
+//         // Add the author to the book object
+//         book.author = author;
+//         // Return the book object
+//         return book;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// async function init() {
+//     let result = await getBookAndAuthor(1);
+//     console.log(result);
+// }
+
+// init();
+
+
+// index.js
+// async function to retrieve a specific book's data with author data
+// const getBookAndAuthor = async (id) => {
+//     try {
+//         // Get the book data
+//         const bookUrl = `http://localhost:3000/books/${id}`;
+//         const bookResponse = await fetch(bookUrl);
+//         const book = await bookResponse.json();
+//         // Get the author data
+//         const authorUrl = `http://localhost:3000/authors/${book.authorId}`;
+//         const authorResponse = await fetch(authorUrl);
+//         const author = await authorResponse.json();
+//         // Add the author to the book object
+//         book.author = author;
+//         // Return the book object
+//         return book;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+//
+// // async IIFE (Immediately Invoked Function Expression)
+// (async () => { // <-- this is our main thread
+//     const book = await getBookAndAuthor(1);
+//     console.log(book);
+// })();
+
+// books-api.js
+export const getBookAndAuthor = async (id) => { // <-- notice the export keyword
+    try {
+        // Get the book data
+        const bookUrl = `http://localhost:3000/books/${id}`;
+        const bookResponse = await fetch(bookUrl);
+        const book = await bookResponse.json();
+        // Get the author data
+        const authorUrl = `http://localhost:3000/authors/${book.authorId}`;
+        const authorResponse = await fetch(authorUrl);
+        const author = await authorResponse.json();
+        // Add the author to the book object
+        book.author = author;
+        // Return the book object
+        console.log(book);
+        return book;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getBookAndAuthor(1);
+
+// json-server --watch data/books.json --port 3000
+
+
+
+
